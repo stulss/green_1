4bb7764dc45f126099d3511a1841e2e0344ff749
stulss <doffl9737@gmail.com>
2023-08-09 18:33:51 +0900

    2023 8/9 수업

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,16 +1,48 @@
 #include <stdio.h> //<stdio.h> 라이브러리(스탠다드 인풋아웃풋/표준 인풋아웃풋)
+#include <string>
 //#include <iostream>//<iostream> 라이브러리
+
+//using namespace std;
+
+/*
+int main(void)
+{
+
+	printf("Hello World");
+
+
+//printf("%d\n", sizeof(char));   //데이터 타입
+//printf("%d\n", sizeof(string)); //데이터 타입
+//printf("%d\n", sizeof(short));  //데이터 타입
+//printf("%d\n", sizeof(int));    //데이터 타입
+//printf("%d\n", sizeof(float));  //데이터 타입 소수점7자리까지
+//printf("%d\n", sizeof(double)); //데이터 타입 소수점8자리까지
+//printf("%d\n", sizeof(long));   //데이터 타입
+
+
+	int user;
+	int user;
+
+
+
+return 0;
+}
+*/
+
+
+/*
+1비트는 0과1로 표시되고 8비트가 모여서 1바이트가 된다. 1바이트는 문자를 표시한다
+*/
+
 
 int main(void)
 {
-printf("Hello World");
+	//char str[128] = "123456789";
 
-printf();//데이터 타입
-char;
-short;
-int;
-long;
-float;
+
+	//printf("%s\n", str);
+
+	
 
 
 
@@ -18,8 +50,42 @@ float;
 return 0;
 }
 
+/*
+[&] and 그리고
+[|] or  또는
+[^] xor 다를때
+[!] not 아니다
+*/
+
+// + 더하기
+// - 빼기
+// * 곱하기
+// / 나누기
+// % 나머지
+// = 대입연산자
+
+// ** 연산자의 결합
+// += 더하기
+// -= 빼기
+// *= 곱기
+// /= 나누기
+// %= 나머지
 
 
+
+
+
+
+//() 소나머지괄호
+//{} 중괄호
+//[] 대괄호
+
+// ; 세미콜론
+// : 콜론
+
+
+// "" 문자열 표시
+// '' 문자   표시
 
 
 
