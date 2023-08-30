0e695358639b49bac07907bbb5fb8bfd45ba642e
sejinpark2 <doffl9737@gmail.com>
2023-08-30 12:41:08 +0900

    8월30일오전수업

--- tses/Main.java
+++ tses/Main.java
@@ -8,10 +8,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        Test test = new Test();
-        test.Initialize();
-        test.Update();
+        User user = new User();
+
+        String name = new String("홍주형");
+        int age = 22;
+        String password = new String("123123");
+        user.setName(name);
+        user.setPassword(password);
+        user.setAge(age);
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
+        System.out.println(user.getAge());
+
 
     }
 
 }
+
+
+/*
+delete = 해제
+new로 동적할당을 했으면 delete로 해제를 해줘야 한다. 안그러면 메모리누수가 일어난다.
+gc = 가비지 컬렉터
+ */
---
+++ tses/User.java
@@ -0,0 +1,62 @@
+import java.util.*;
+public class User {
+
+    private String name;
+    private String password;
+    private int age;
+
+    private Address address;
+
+
+    public User(){
+        this.name="";
+        this.password ="";
+        this.age=0;
+        this.address=new Address();
+    }
+
+    public User(User user){this(user.name,user.password,user.age);}
+
+    public  User(String password, int age){
+        this("",password,age);
+    }
+
+    public User(String name,String password,int age) {
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+    public User setAddress(Address address)
+    {
+        this.address = address;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
