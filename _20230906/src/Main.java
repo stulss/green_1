1ee0eae1c70d031cef9ec16df4b3739891452c14
stulss <doffl9737@gmail.com>
2023-09-06 18:18:56 +0900

    9월6일수업

---
+++ _20230906/src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainUpdate Main = new MainUpdate();
+        Main.render();
+
+    }
+}
---
+++ _20230906/src/MainUpdate.java
@@ -0,0 +1,5 @@
+public class MainUpdate {
+
+    public void render() {
+    }
+}
